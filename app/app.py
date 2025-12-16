77a129497697c351e3ab9f07306584793596122a
Ryan <ryan.marsh119@gmail.com>
2025-12-15 19:16:14 -0500

    Added get by id and delete endpoints.

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from app.db import PracticeSession, get_async_session, create_db_and_tables
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+import uuid
 # from .routers import 
 
 @asynccontextmanager
@@ -23,6 +24,22 @@ async def get_logs(
 
     return [SavedPracticeSession.model_validate(all, from_attributes=True) for all in all_logs]
 
+@app.get('/logs/{id}')
+async def get_logs(
+    id: str,
+    db: AsyncSession = Depends(get_async_session)
+) ->SavedPracticeSession:
+    
+    log_uuid = uuid.UUID(id)
+    stmt = select(PracticeSession).where(PracticeSession.id==log_uuid)
+    result = await db.scalars(stmt)
+    log = result.one_or_none()
+
+
+    if not log:
+        raise HTTPException(status_code=404, detail="Practice session not found!")
+
+    return SavedPracticeSession.model_validate(log, from_attributes=True)
 
 
 @app.post('/newlog')
@@ -41,3 +58,23 @@ async def newlog(
 
     except Exception as e:
         raise HTTPException(status_code=404, detail=f"Session not saved. {str(e)}")
+    
+
+@app.delete('/deletelog', status_code=204)
+async def deletelog(
+    id: str,
+    db: AsyncSession = Depends(get_async_session)
+) -> None:
+
+    log_uuid = uuid.UUID(id)
+
+    stmt = select(PracticeSession).where(PracticeSession.id == log_uuid)
+    result = await db.scalars(stmt)
+    deleted_log = result.one_or_none()
+
+    if not deleted_log:
+        raise HTTPException(status_code=404, detail="Practice session not found!")
+    
+    await db.delete(deleted_log)
+    await db.commit()
+
